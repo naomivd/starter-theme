6df971ebd342dbf225c315b39a8165696e9d8107
Naomi <naomivd@gmail.com>
2015-02-08 14:40:29 +0100

    new page templates

---
+++ page-full-width.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * This is a full width page template (no sidebar).
+ *
+ * @package Starter_Theme
+ */
+
+get_header(); ?>
+
+<section id="primary" class="full-width" role="main">
+
+   <!-- Everything in here is the same as the default template shown above -->
+
+</section><!-- #primary -->
+
+<?php get_footer(); ?>
